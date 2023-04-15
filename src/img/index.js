cb7743b5f8c548203be813e18ba2ab3cec56f74f
adislksn <adi.120140038@student.itera.ac.id>
2023-04-15 10:45:00 +0700

    Fixing

--- src/img/index.js
+++ src/img/index.js
@@ -1,5 +1,5 @@
-export const BackgroundDesktop = require('./BluePurpbknd.png');
-export const BackgroundMobile = require('./BluePurpMobile.png');
+export const BackgroundDesktop = require('./BluePurpbknd.webp');
+export const BackgroundMobile = require('./BluePurpMobile.webp');
 export const logo = require('./KM_logo.png');
 export const logoKM = require('./logoKM_320x320.png');
 export const logoITERA = require('./logoItera_320x320.png');
