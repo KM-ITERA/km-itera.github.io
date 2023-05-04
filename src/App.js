31ae8251a8cd25909b4beeb591187a81019293f0
adislksn <adi.120140038@student.itera.ac.id>
2023-05-04 16:53:07 +0700

    Update list

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import {
     logoITERA,
     logoKM
 } from './img/index'
-// import AccordionList from './components/accordionList';
 import { AccordionList, BtnList } from './components/index';
 import './App.css';
 import './index.css';
@@ -59,7 +58,7 @@ const items = [
     },
     {
         "judul": "Duta ITERA",
-        "tanggal": "1 - 3 Mei 2023",
+        "tanggal": "1 - 6 Mei 2023",
         "details1":"Duta ITERA merupakan ajang pemilihan mahasiswa dan mahasiswi terbaik yang ada di ITERA yang dimana bertujuan untuk mencari mahasiswa/i dari ITERA yang mampu mengenalkan ITERA ke masyarakat luas serta menjadi FACE OF ITERA itu sendiri dan role model bagi mahasiswa/i lainnya dengan kriteria yang sudah ditentukan",
         "details2": "",
         "daftar": "https://bit.ly/FormPendaftaranKetuaPelaksanaDUTA-ITERA2023",
@@ -67,26 +66,8 @@ const items = [
         "status": "text-goldish"
     },
     {
-        "judul": "ITERA Open", 
-        "tanggal": "26 April - 2 Mei 2023", 
-        "details1": 'ITERA OPEN merupakan pesta olahraga yang diselenggarakan oleh Institut Teknologi Sumatera, dengan menargetkan mahasiswa dan mahasiswi dari perguruan tinggi yang ada di wilayah lampung dan sumatera, untuk turut serta berpartisipasi di dalam kompetisi. Kegiatan hadir sebagai salah satu upaya memperkenalkan kampus Institut Teknologi Sumatera sebagai kampus muda yang mendukung penuh keolahragaan.',
-        "details2": 'Dengan ini, ITERA OPEN diharapkan dapat menjadi momentum dalam menjalin tali silaturahmi serta ajang kompetisi antarkampus, yang diharapkan dapat membawa nama baik dan citra kampus masing-masing dengan menjunjung tinggi nilai sportifitas dan profesionalitas selama berkompetisi.',
-        "daftar": "https://bit.ly/pendaftaranketuplakITERAOPEN2023",
-        "syarat": "https://bit.ly/SyaratdanKetentuanCalonKetuaPelaksanaITERAOPEN2023",
-        "status": "text-goldish"
-    },
-    {
-        "judul": "Liga Ilmiah ITERA (LITERA)",
-        "tanggal": "28 April - 2 Mei 2023",
-        "details1":"LITERA atau Liga Ilmiah ITERA merupakan suatu ajang perlombaan ilmiah terbesar yang dilaksanakan oleh Kementerian Pendidikan, Riset, dan Teknologi Kabinet Wangsabatih KMITERA pada tahun ini. LITERA dilaksanakan sebagai kegiatan kompetisi keilmiahan di lingkungan kampus ITERA yang bertujuan dalam menumbuhkan rasa berpikir kritis serta jiwa kompetitif pada mahasiswa/i di ITERA dan yang diakhiri oleh acara puncak berupa Malam Apresiasi bagi seluruh peserta LITERA.",
-        "details2": "LITERA tidak hanya bertujuan untuk menjadi tempat para mahasiswa/i ITERA dalam meningkatkan kemampuan kognitif namun juga dapat menjadi tempat silaturahmi serta mengembangkan menjalin rasa kekeluargaan bagi mahasiswa/i dari berbagai program studi di ITERA.",
-        "daftar": "https://docs.google.com/forms/d/e/1FAIpQLSdZUiocZti98PYrYZ3dq-NeCHGY9ch8nWAxco4PlGCt7KJGkw/viewform?usp=sf_link",
-        "syarat": "https://drive.google.com/drive/folders/1pEOCA_eEs6cmfIsxZkNQg8rT47QSS-wU",
-        "status": "text-goldish"
-    },
-    {
         "judul": "Riuh Wisuda Terpusat",
-        "tanggal": "29 April - 2 Mei 2023",
+        "tanggal": "29 April - 4 Mei 2023",
         "details1":"Kegiatan ini adalah kegiatan yang diadakan secara terpusat atau satu komando dalam satu kesatuan seluruh elemen organisasi mahasiswa yang ada di ITERA, sebagai arak-arakan wisuda yang dibuat dalam rangka mengapresiasi para wisudawan/wisudawati.",
         "details2": "",
         "daftar": "https://bit.ly/PendaftaranKetuplakRiuhWisudaTerpusat2023",
@@ -104,6 +85,24 @@ const items = [
     },
     {
         "judul": "LKMM-TD",
+        "tanggal": "CLOSED...",
+        "details1":"",
+        "details2": "",
+        "daftar": "",
+        "syarat": "",
+        "status": "text-gray-400"
+    },
+    {
+        "judul": "ITERA Open", 
+        "tanggal": "CLOSED...", 
+        "details1": "",
+        "details2": "",
+        "daftar": "",
+        "syarat": "",
+        "status": "text-gray-400"
+    },
+    {
+        "judul": "Liga Ilmiah ITERA (LITERA)",
         "tanggal": "CLOSED...",
         "details1":"",
         "details2": "",
